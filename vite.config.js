ad051bbb85ec87c8b0a6f71c6f1517aa790ca957
amPRaj <praveenraj91318@gmail.com>
2025-03-16 17:00:31 +0530

    final

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ["**/*.jpg", "**/*.JPG", "**/*.jpeg"], // Include image assets
+  build: {
+    outDir: "dist", // Ensure build files go into "dist" folder
+    emptyOutDir: true, // Clears old files before building
+  },
 });
